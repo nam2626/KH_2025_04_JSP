72cf6064355dd5a3f17749c15eac5939e6d3e842
nam2626 <nam2626@naver.com>
2025-05-28 10:22:51 +0900

    jdbc

--- JDBC/src/jdbc/JDBCTest1.java
+++ JDBC/src/jdbc/JDBCTest1.java
@@ -2,7 +2,9 @@ package jdbc;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class JDBCTest1 {
 
@@ -13,11 +15,26 @@ public class JDBCTest1 {
 			System.out.println("드라이버 로딩 완료");
 			//2. 데이터베이스 접속
 //			Connection conn = 
-//					DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe",
-//							"c##scott","123456");
+//					DriverManager.getConnection("jdbc:oracle:thin:c##scott/123456@localhost:1521:xe");
+
 			Connection conn = 
-					DriverManager.getConnection("jdbc:oracle:thin:c##scott/123456@localhost:1521:xe");
+					DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe",
+							"c##scott","123456");
 			System.out.println("데이터 베이스 접속 완료");
+			//3. SQL문 작성
+			String sql = "select * from student";
+			//4. Statement 생성(SQL문을 데이터베이스 전송하고 실행하는 객체)
+			Statement stmt = conn.createStatement();
+			//5. SQL문 실행
+			ResultSet rs = stmt.executeQuery(sql);
+			//6. 결과 출력
+			while(rs.next()) {
+				System.out.println(
+						rs.getString(1) + " " + rs.getString("sname") +
+						" " + rs.getDouble("score") + " " + rs.getString("gender")
+						);
+			}
+			
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
 		} catch (SQLException e) {
@@ -25,7 +42,6 @@ public class JDBCTest1 {
 		}
 		
 		
-		//3. SQL문 작성
 		
 		//4. SQL문 실행
 		
