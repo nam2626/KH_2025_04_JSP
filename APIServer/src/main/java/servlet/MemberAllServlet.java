d5609c5845daef5c19b7188446cfe91e52efbf46
nam2626 <nam2626@naver.com>
2025-06-27 17:11:43 +0900

    데이터 전달 및 서블릿에서 json 파싱

--- APIServer/src/main/java/servlet/MemberAllServlet.java
+++ APIServer/src/main/java/servlet/MemberAllServlet.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -43,7 +44,28 @@ public class MemberAllServlet extends HttpServlet {
 		response.getWriter().println(jsonObject.toString());
 	}
 
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		//Stream API이용해서 한번 본문 내용 읽어오기
+		String requestBody =
+				req.getReader().lines().collect(
+						Collectors.joining(System.lineSeparator()));
+		System.out.println(requestBody);
+		//JSON으로 파싱 후 회원정보를 등록
+		JSONObject json = new JSONObject(requestBody);
+		System.out.println(json.getString("id"));
+		System.out.println(json.getString("passwd"));
+		System.out.println(json.getString("userName"));
+		System.out.println(json.getString("nickName"));
+		//마이바티스로 데이터 추가하고 추가한 결과를 클라이언트에게 전달
+		//sql 결과가 1 이면 메세지 : 회원정보 등록 완료
+		//          0 이면 메세지 : 회원정보 등록 실패
+		
+	}
 
+
+	
+	
 }
 
 
