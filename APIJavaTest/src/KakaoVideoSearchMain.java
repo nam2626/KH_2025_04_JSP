03fb9eec5eb7269a314c3ae6b1eba4c9812845b2
nam2626 <nam2626@naver.com>
2025-07-02 09:56:46 +0900

    API 호출

--- APIJavaTest/src/KakaoVideoSearchMain.java
+++ APIJavaTest/src/KakaoVideoSearchMain.java
@@ -2,44 +2,38 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLEncoder;
-import java.nio.charset.StandardCharsets;
 import java.util.stream.Collectors;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
 
-public class PostAPICallMain {
-	public static String callAPI(String id, String passwd, 
-									String userName, String nickName) {
+public class KakaoVideoSearchMain {
+	private static String REST_API_KEY = "d77c810e173c3afbf300da53d245b731";
+	
+	public static String callKakaoAPI(String searchText) {
 		String responseBody = "";
 //		1. URL 셋팅
-		String apiURL = "http://localhost:8888/APIServer/members";
-
+		String apiURL = "https://dapi.kakao.com/v2/search/vclip";
+		try {
+			searchText = URLEncoder.encode(searchText, "UTF-8");
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
+		apiURL += "?query=" + searchText;
 		HttpURLConnection conn = null;
 		try {
 			URL url = new URL(apiURL);
 			//연결 객체 생성
 			conn = (HttpURLConnection) url.openConnection();
 //		2. 헤더에 인증 및 기타 정보 셋팅
-			conn.setRequestMethod("POST");
-			//데이터 전송을 위해서 output 설정
-			conn.setDoOutput(true);
-			
-			//전달할 회원 정보 JSONObject 생성
-			JSONObject json = new JSONObject();
-			json.put("id", id);
-			json.put("passwd", passwd);
-			json.put("nickName", nickName);
-			json.put("userName", userName);
+			conn.setRequestMethod("GET");
+			conn.setRequestProperty("Authorization", "KakaoAK d77c810e173c3afbf300da53d245b731");
 
-			//서버로 데이터 전송
-			conn.getOutputStream().write(json.toString().getBytes(StandardCharsets.UTF_8));
-			
 			InputStream is = null;
 			//3. 요청
 			//4. 요청이 정상적으로 처리가 되었는지 체크
@@ -66,10 +60,21 @@ public class PostAPICallMain {
 		return responseBody;
 	}
 	public static void main(String[] args) {
-		String result = callAPI("admin1234", "123456", "사용자", "닉네임");
+		String result = callKakaoAPI("해수욕장");
+//		5. 화면에 받아온 데이터를 출력
 		System.out.println(result);
+		
+		JSONObject json = new JSONObject(result);
+		JSONArray arr = json.getJSONArray("documents");
+		arr.forEach(item -> {
+			JSONObject obj = (JSONObject) item;
+			System.out.println(obj.getString("title"));
+			System.out.println(obj.getString("datetime"));
+			System.out.println(obj.getString("thumbnail"));
+			System.out.println(obj.getString("url"));
+			System.out.println("-------------------------------");
+		});
 	}
-
 }
 
 
